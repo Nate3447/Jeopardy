464a78db5ae254c99b0900dfd25a4a1dfc902900
Nate3447 <nathaniel.sierras@gmail.com>
2019-03-26 20:57:53 +0800

    Finished Main.java

--- NETWORK Jeopardy/src/Controller/Main.java
+++ NETWORK Jeopardy/src/Controller/Main.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import Model.*;
 
@@ -11,17 +12,31 @@ public class Main {
 		UDPClient client;
 		UDPServer server;
 		
+		System.out.println("Host a game (Press Y)\nJoin a match (Press N)\n");	
+
+		Scanner scan = new Scanner (System.in);
+		String input;
+		boolean isServer = false;
+		boolean inputCorrect = false;
 		
-		boolean isServer = true;
+		while(!inputCorrect) {
+			 input = scan.nextLine();
+			 if (input.equals("Y") || input.equals("y")) {
+				 isServer = true;
+			 } else if (input.equals("N") || input.contentEquals("n")) {
+				 isServer = false;
+			 } else {
+				 System.out.println("\nIncorrect command.\n\n");
+				 System.out.println("Host a game (Press Y)\nJoin a match (Press N)\n");
+			 }
+		}
 		
 		if (isServer) {
 			server = new UDPServer();
 			server.startServer();
-			
 		} else {
-			
 			client = new UDPClient();
-			
+			client.startClient();
 		}
 		
 	}
