8995107069ab5f25eea8547b349b17d8a5adc4ca
Nate3447 <nathaniel.sierras@gmail.com>
2019-03-26 14:51:34 +0800

    Moved server code in Main to UDPServer

--- NETWORK Jeopardy/src/Controller/UDPServer.java
+++ NETWORK Jeopardy/src/Controller/UDPServer.java
@@ -2,8 +2,16 @@ package Controller;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+
+import Model.Jeopardy;
+import Model.Question;
 
 public class UDPServer {
+	
+	private Jeopardy game;
+	private ArrayList<Question> questions;
+	
 	
 	public void startServer() throws Exception {
 		
@@ -39,5 +47,39 @@ public class UDPServer {
 		
 	}
 	
+	public void initGame() {
+		game = new Jeopardy();
+		questions = new ArrayList<Question>();
+		
+		questions.add(new Question("Math", "Bringing two or more numbers together to make a new total.", "addition", 100, false));
+		questions.add(new Question("Math", "Splitting into equal parts or groups.", "division", 100, false)); 
+		questions.add(new Question("Math", "Taking one number away from another.", "subtraction", 100, false)); 
+		questions.add(new Question("Math", "The sum of all given values, divided by the amount of values given.", "average", 200, false)); 
+		questions.add(new Question("Math", "A factor that occurs in all the numbers being compared.", "common factor", 200, false)); 
+		questions.add(new Question("Math", "A multiple that occurs in all the numbers being compared.", "common multiple", 200, false)); 
+		questions.add(new Question("Math", "The bottom number of a fraction.", "denominator", 300, false)); 
+		questions.add(new Question("Math", "A fraction with a numerator that is larger than the denominator.", "improper fraction", 300, false)); 
+		questions.add(new Question("Math", "The result of multiplication.", "product", 300, false));
+		questions.add(new Question("Science", "A test performed to verify a hypothesis.", "experiment", 100, false));
+		questions.add(new Question("Science", "A guess about the result of an experiment.", "hypothesis", 100, false));
+		questions.add(new Question("Science", "The factor in an experiment that does not change.", "control", 100, false));
+		questions.add(new Question("Science", "The green pigment found in various plants.", "chlorophyll", 200, false));
+		questions.add(new Question("Science", "The process by which species change to adapt to their environment.", "evolution", 200, false));
+		questions.add(new Question("Science", "The organelle often referred to as the brain of the cell.", "nucleus", 200, false));
+		questions.add(new Question("Science", "DNA molecules that contain the set of instructions required to build a cell.", "chromosomes", 300, false));
+		questions.add(new Question("Science", "The observable characteristics of an individual.", "phenotype", 300, false));
+		questions.add(new Question("Science", "The number of protons in an atom.", "atomic number", 300, false));
+		questions.add(new Question("English", "The name of a person, place, or thing.", "noun", 100, false));
+		questions.add(new Question("English", "A word used to refer to a noun, usually to avoid repetition.", "pronoun", 100, false));
+		questions.add(new Question("English", "A word used to describe a noun or pronoun.", "adjective", 100, false));
+		questions.add(new Question("English", "A joining word.", "conjunction", 200, false));
+		questions.add(new Question("English", "A verb form ending in -ing and used as a noun.", "gerund", 200, false));
+		questions.add(new Question("English", "A word derived from a verb and having the qualities of both a verb and adjective; never used as a noun.", "participle", 200, false));
+		questions.add(new Question("English", "A group of words containing a subject and its verb.", "clause", 300, false));
+		questions.add(new Question("English", "An exclamation inserted into an utterance without grammatical connection.", "interjection", 300, false));
+		questions.add(new Question("English", "A group of words not containing a subject and its verb", "phrase", 300, false));
+		
+		game.setQuestions(questions);
+	}
 	
 }
